1a42475fc54be818b21c84187f73235ede5285f0
ramyb <rami@dudamobile.com>
2015-03-31 02:49:18 +0300

    support disabled option

--- addon/services/totango.js
+++ addon/services/totango.js
@@ -5,8 +5,6 @@ import {
 from 'ember-cli-totango/utils/error-messages';
 import Ember from 'ember';
 
-
-
 export default (Ember.Service || Ember.Object).extend({
 	initDeferred: function() {
 		this.set('_deferred', Ember.RSVP.defer());
@@ -14,22 +12,27 @@ export default (Ember.Service || Ember.Object).extend({
 	loadScript: function() {
 		var rejectFn = this.get('_deferred').reject,
 			resolveFn = this.get('_deferred').resolve;
-		if (!this.get('scriptUrl')) {
-			rejectFn(new Error(MISSING_SCRIPT_URL));
-			return;
-		}
-		if (!this.get('totangoOptions')) {
-			rejectFn(new Error(MISSING_TOTANGO_OPTIONS));
-			return;
-		}
-		window.totango_options = this.get('totangoOptions');
-		if (this.get('scriptUrl')) {
-			injectScript(this.get('scriptUrl'))
-				.then(Ember.run.bind(this, function() {
-					var totango = window.totango;
-					this.set('_totango', window.totango);
-					resolveFn(totango);
-				}))['catch'](Ember.run.bind(null, rejectFn));
+		if (!this.get('disabled')){
+			if (!this.get('scriptUrl')) {
+				rejectFn(new Error(MISSING_SCRIPT_URL));
+				return;
+			}
+			if (!this.get('totangoOptions')) {
+				rejectFn(new Error(MISSING_TOTANGO_OPTIONS));
+				return;
+			}
+			window.totango_options = this.get('totangoOptions');
+			if (this.get('scriptUrl')) {
+				injectScript(this.get('scriptUrl'))
+					.then(Ember.run.bind(this, function() {
+						var totango = window.totango;
+						this.set('_totango', window.totango);
+						resolveFn(totango);
+					}))['catch'](Ember.run.bind(null, rejectFn));
+			}	
+		} else {
+			this.set('_totango', {disabled: true});
+			resolveFn(this.get('_totango'));
 		}
 	},
 	promise: Ember.computed(function() {
@@ -41,6 +44,7 @@ export default (Ember.Service || Ember.Object).extend({
 	}),
 	scriptUrl: '//s3.amazonaws.com/totango-cdn/totango2.js',
 	totangoOptions: null,
+	disabled: false,
 
 	_deferred: null,
 	_totango: null
--- app/services/totango.js
+++ app/services/totango.js
@@ -1,7 +1,5 @@
 import config from '../config/environment';
 import TotangoService from 'ember-cli-totango/services/totango';
 import Ember from 'ember';
-var totangoOptions = Ember.get(config, 'ember-cli-totango.totangoOptions');
-export default TotangoService.extend(totangoOptions ? {
-	totangoOptions: totangoOptions
-} : {});
+var emberCliTotangoConfig = Ember.get(config, 'ember-cli-totango') || {disable: true};
+export default TotangoService.extend(emberCliTotangoConfig);

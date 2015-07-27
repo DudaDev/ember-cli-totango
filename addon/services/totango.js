61e4c638b1b982162d5bb68fdb648309d6711472
ramyb <rami@dudamobile.com>
2015-07-27 17:59:23 +0300

    some nicer syntax

--- addon/services/totango.js
+++ addon/services/totango.js
@@ -1,26 +1,44 @@
-import Ember from 'ember';
+import Em from 'ember';
 import injectScript from 'ember-inject-script';
 import {
 	MISSING_SCRIPT_URL, MISSING_TOTANGO_OPTIONS
 }
 from 'ember-cli-totango/utils/error-messages';
 import defaults from 'ember-cli-totango/utils/defaults';
+import $ from 'jquery';
 
-var defaultsMixin = Ember.Mixin.create(Ember.$.extend({}, defaults));
+const {
+		extend
+	} = $, 
+	{
+		Mixin,
+		computed,
+		run : {
+			bind
+		},
+		on,
+		RSVP: {
+			defer
+		},
+		Service
+	} = Em;
 
-export default (Ember.Service || Ember.Object).extend({
+var defaultsMixin = Mixin.create(extend({}, defaults));
+
+export default (Service || Em.Object).extend({
+
 	_deferred: null,
 
 	_totango: null,
 
-	initDeferred: function() {
-		this.set('_deferred', Ember.RSVP.defer());
-	}.on('init'),
+	initDeferred: on('init', function() {
+		this.set('_deferred', defer());
+	}),
 
 	loadScript: function() {
 		var rejectFn = this.get('_deferred').reject,
 			resolveFn = this.get('_deferred').resolve;
-		if (!this.get('disabled')){
+		if (!this.get('disabled')) {
 			if (!this.get('scriptUrl')) {
 				rejectFn(new Error(MISSING_SCRIPT_URL));
 				return;
@@ -32,31 +50,33 @@ export default (Ember.Service || Ember.Object).extend({
 			window.totango_options = this.get('totangoOptions');
 			if (this.get('scriptUrl')) {
 				injectScript(this.get('scriptUrl'))
-					.then(Ember.run.bind(this, function() {
+					.then(bind(this, function() {
 						var totango = window.totango;
 						this.set('_totango', window.totango);
 						resolveFn(totango);
-					}))['catch'](Ember.run.bind(null, rejectFn));
-			}	
+					}))['catch'](bind(null, rejectFn));
+			}
 		} else {
-			this.set('_totango', {disabled: true});
+			this.set('_totango', {
+				disabled: true
+			});
 			resolveFn(this.get('_totango'));
 		}
 	},
 
-	promise: Ember.computed(function() {
+	promise: computed(function() {
 		this.loadScript();
 		return this.get('_deferred').promise;
 	}),
 
-	instance: Ember.computed(function() {
+	instance: computed(function() {
 		return this.get('_totango');
 	}),
 
 	scriptUrl: null,
 
 	totangoOptions: null,
-	
+
 	disabled: false
 
 }, defaultsMixin);
--- addon/utils/error-messages.js
+++ addon/utils/error-messages.js
@@ -1,5 +1,5 @@
 var MISSING_SCRIPT_URL 			= 'Totango script url is missing',
-	MISSING_TOTANGO_OPTIONS 	='Totango options are missing';
+	MISSING_TOTANGO_OPTIONS 	= 'Totango options are missing';
 
 export {
 	MISSING_SCRIPT_URL,
